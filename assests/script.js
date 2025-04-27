a11d6c08f96617806184573c53b9ca01faab370b
logeshprabulp <logeshprabulp@gmail.com>
2025-04-27 20:25:18 +0530

    version 2

--- assests/script.js
+++ assests/script.js
@@ -1,73 +1,37 @@
-// Dark Mode
-const moonIcon = document.getElementById("moon-icon");
-const sunIcon = document.getElementById("sun-icon");
+// Year 
+document.getElementById('year').textContent = new Date().getFullYear();
+ // Back to top
+ var amountScrolled = 500;
+ var amountScrolledNav = 25;
 
-function setTheme(isMoon) {
-  if (isMoon) {
-    document.documentElement.style.setProperty("--primary-color", "#FFFFFF");
-    document.documentElement.style.setProperty("--secondary-color", "#2A2B2D");
-  } else {
-    document.documentElement.style.setProperty("--primary-color", "#2A2B2D");
-    document.documentElement.style.setProperty("--secondary-color", "#FFFFFF");
-  }
-}
-setTheme(false);
+ $(window).scroll(function () {
+   if ($(window).scrollTop() > amountScrolled) {
+     $("button.back-to-top").addClass("show");
+   } else {
+     $("button.back-to-top").removeClass("show");
+   }
+ });
 
-moonIcon.addEventListener("click", () => {
-  moonIcon.style.display = "none";
-  sunIcon.style.display = "inline-block";
-  setTheme(true);
-});
-
-sunIcon.addEventListener("click", () => {
-  sunIcon.style.display = "none";
-  moonIcon.style.display = "inline-block";
-  setTheme(false);
-});
-
-// Marquee
-function Marquee(selector, speed) {
-  const parentSelector = document.querySelector(selector);
-  const clone = parentSelector.innerHTML;
-  const firstElement = parentSelector.children[0];
-  let i = 0;
-  console.log(firstElement);
-  parentSelector.insertAdjacentHTML("beforeend", clone);
-  parentSelector.insertAdjacentHTML("beforeend", clone);
-
-  setInterval(function () {
-    firstElement.style.marginLeft = `-${i}px`;
-    if (i > firstElement.clientWidth) {
-      i = 0;
-    }
-    i = i + speed;
-  }, 0);
-}
-window.addEventListener("load", Marquee(".marquee", 0.2));
-
-// Year
-const d = new Date();
-let year = d.getFullYear();
-document.getElementById("year").innerHTML = year;
-
-// owl 
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  dots:false,
-  navText: ['<i class="fa fa-angle-left gallery-nav-left" aria-hidden="true"></i>',
-    '<i class="fa fa-angle-right gallery-nav-right" aria-hidden="true"></i>'],
-  loop:false,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:1
-      },
-      1000:{
-          items:1
-      }
-  }
+ $("button.back-to-top").click(function () {
+   $("html, body").animate(
+     {
+       scrollTop: 0,
+     },
+     800
+   );
+   return false;
+ });
+//toggle
+var toggle = document.getElementById("navIcon")
+var sidenav = document.getElementById("sidenavId")
+var toggleClose = document.getElementById("navIconClose")
+toggle.addEventListener("click",()=>{
+  sidenav.style.display = "block";
+  toggle.style.display='none';
+  toggleClose.style.display='block';
+})
+toggleClose.addEventListener("click",()=>{
+  sidenav.style.display = "none";
+  toggleClose.style.display='none';
+  toggle.style.display='block';
 })
